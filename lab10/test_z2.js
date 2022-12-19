bc3baa8b3fa64cbc75f55f781daf98cb7d279640
jurbanska <jurbanka@student.agh.edu.pl>
2022-12-19 16:20:28 +0100

    task 2 done

--- lab10/test_z2.js
+++ lab10/test_z2.js
@@ -2,26 +2,26 @@ const assert = require('assert');
 const fs = require('fs');
 
 describe('fsTest', () => {
-    it('Sprawdza czy plik o podanej ścieżce istnieje', () => {
-        const path = '/home/jurbanska/WebstormProjects/Programowanie_skryptowe/lab10/plik.txt';
-        const expected = true;
-        const actual = fs.existsSync(path);
-        assert.equal(actual, expected);
-    });
+  it('Sprawdza czy plik o podanej ścieżce istnieje', () => {
+    const path = '/home/jurbanska/WebstormProjects/Programowanie_skryptowe/lab10/plik.txt';
+    const expected = true;
+    const actual = fs.existsSync(path);
+    assert.equal(actual, expected);
+  });
 
-    it('Sprawdza czy podany plik jest plikiem', () => {
-        const path = '/home/jurbanska/WebstormProjects/Programowanie_skryptowe/lab10/plik.txt';
-        const expected = true;
-        const stat = fs.statSync(path);
-        const actual = stat.isFile();
-        assert.equal(actual, expected);
-    });
+  it('Sprawdza czy podany plik jest plikiem', () => {
+    const path = '/home/jurbanska/WebstormProjects/Programowanie_skryptowe/lab10/plik.txt';
+    const expected = true;
+    const stat = fs.statSync(path);
+    const actual = stat.isFile();
+    assert.equal(actual, expected);
+  });
 
-    it('Sprawdza czy podany plik jest katalogiem', () => {
-        const path = '/home/jurbanska/WebstormProjects/Programowanie_skryptowe/lab10';
-        const expected = true;
-        const stat = fs.statSync(path);
-        const actual = stat.isDirectory();
-        assert.equal(actual, expected);
-    });
+  it('Sprawdza czy podany plik jest katalogiem', () => {
+    const path = '/home/jurbanska/WebstormProjects/Programowanie_skryptowe/lab10';
+    const expected = true;
+    const stat = fs.statSync(path);
+    const actual = stat.isDirectory();
+    assert.equal(actual, expected);
+  });
 });
